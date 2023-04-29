15fd01c46a8ca0c3f623b19f55084d8967f5df04
Andrew Bish <aebish@mail.fhsu.edu>
2023-04-29 15:07:44 -0700

    added html, css, cors, middleware

--- ExpressServer.js
+++ ExpressServer.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const corsOptions = require("./config/corsOptions");
 const mongoose = require("mongoose"); // <-- add require here
 const connectDB = require("./config/dbConfig");
 const PORT = process.env.PORT || 3000;
 
 //connect to DB
 connectDB();
+// logger in middleware
+app.use(logger);
+//cors
+app.use(cors(corsOptions));
 
-
+// built in middleware to handel urlencoded data
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
 mongoose.connection.once("open", () => {
