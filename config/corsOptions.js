f90dd51e7713dedd5bfad608b2025a01456fa381
Andrew Bish <aebish@mail.fhsu.edu>
2023-04-29 22:30:42 -0700

    whitelist for glitch and package

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,6 +3,8 @@ const whiteList = [
     "https://www.google.com",
     "http://127.0.0.1:5500",
     "http://localhost:3000",
+    "https://example.com",
+    "https://glitch.com"
   ];
   
   const corsOptions = {
